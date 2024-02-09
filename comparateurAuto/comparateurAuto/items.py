9d1c079caef9eb65e2cd433066fb02196ac51df3
Matias gomez <mrmatiasgomez@gmail.com>
2024-02-09 11:40:15 +0100

    Modif item

--- comparateurAuto/comparateurAuto/items.py
+++ comparateurAuto/comparateurAuto/items.py
@@ -1,11 +1,11 @@
 import scrapy
 
 class ComparateurautoItem(scrapy.Item):
+    brand = scrapy.Field()
     model = scrapy.Field()
     year = scrapy.Field()
     mileage = scrapy.Field()
     engine_type = scrapy.Field()
-    price = scrapy.Field()
     gearbox = scrapy.Field()
-    brand = scrapy.Field()
+    price = scrapy.Field()
     url = scrapy.Field()
--- comparateurAuto/comparateurAuto/spiders/automobile_fr.py
+++ comparateurAuto/comparateurAuto/spiders/automobile_fr.py
@@ -4,8 +4,10 @@ import scrapy
 class AutomobileFrSpider(scrapy.Spider):
     name = "automobile_fr"
     allowed_domains = ["www.automobile.fr"]
-    start_urls = ["https://www.automobile.fr/"]
+    start_urls = ["https://www.automobile.fr/cat%C3%A9gorie/voiture/vhc:car,pgn:10,pgs:10,dmg:false"]
 
     def parse(self, response):
-        brand = response.css("div::text").extract()
-        pass
+        brand = response.css("article:nth-child(2) > div > div.g-row.js-ad-entry > a > div.g-col-s-12.g-col-m-8 > div.vehicle-text.g-row > h3").extract()
+        print(brand)
+
+
